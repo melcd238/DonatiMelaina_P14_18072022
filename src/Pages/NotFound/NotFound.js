51a6a05b7797526ab2e044b37809a704716f4bdf
melcd238 <melcd238@gmail.com>
2022-07-18 21:39:08 +0200

    not found page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,11 +1,20 @@
 import './NotFound.css'
+import logo from "../../Assets/wealth.png"
+import { Link } from 'react-router-dom'
 
 const NotFound =()=>{
 
     return(
-        <>
-        Not Found
-        </>
+        <section className='not-found'>
+         <h1>Oups!!!! Something wrong happens</h1>
+         <div>
+             <span className='error'>404</span>
+         </div>
+         <div>
+             <img src={logo} alt="logo wealth health"></img>
+         </div>
+          <Link to="/">Come back Home</Link>
+        </section>
     )
 }
 
