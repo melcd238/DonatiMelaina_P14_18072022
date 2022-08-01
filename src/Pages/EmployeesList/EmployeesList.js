7d9dd70eb8672869b7c487c98525ea09ba04d72f
melcd238 <melcd238@gmail.com>
2022-08-01 14:50:57 +0200

    update renderDataTable

--- src/Pages/EmployeesList/EmployeesList.js
+++ src/Pages/EmployeesList/EmployeesList.js
@@ -8,9 +8,42 @@ const EmployeesList = ()=>{
 
     console.log(employees)
 
+    const tableHeader = ()=>{
+        let header = Object.keys(employees[0])
+        console.log(header)
+         return header.map((key, index)=>(
+             <th key={index}>{key.toUpperCase()}</th>
+         ))
+    }
+
+    const renderTableDataEmployees = ()=>{
+        return employees.map((employee, index)=>(
+               <tr key={index}>
+                   <td>{employee.firstName}</td>
+                   <td>{employee.lastName}</td>
+                   <td>{employee.birth}</td>
+                   <td>{employee.startDate}</td>
+                   <td>{employee.street}</td>
+                   <td>{employee.city}</td>
+                   <td>{employee.state}</td>
+                   <td>{employee.zipCode}</td>
+                   <td>{employee.dept}</td>
+               </tr>
+         
+        ))
+    }
+
     return(
         <main className="employees-container">
            <h1>Current Employees</h1>
+          <section>
+              <table id="employees">
+                  <tbody>
+                      <tr>{tableHeader()}</tr>
+                      {renderTableDataEmployees()}
+                  </tbody>
+              </table>
+          </section> 
         </main>
     )
 }
--- src/Store/Reducers/Employees.js
+++ src/Store/Reducers/Employees.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { DEFAULT_EMPLOYEES_LIST } from '../../data/employeesData'
+import { DEFAULT_EMPLOYEES_LIST } from '../../data/employeesData';
 
 
 
